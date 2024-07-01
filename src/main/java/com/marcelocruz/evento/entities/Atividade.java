1e6dc3d38c3d3d464becdf1b74e537c4c3c71b1e
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2024-07-01 14:59:12 -0300

    Entidade Atividade e relacionamento muitos-para-um

--- src/main/java/com/marcelocruz/evento/entities/Atividade.java
+++ src/main/java/com/marcelocruz/evento/entities/Atividade.java
@@ -2,28 +2,30 @@ package com.marcelocruz.evento.entities;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_atividade")
+public class Atividade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String nome;
     private String descricao;
+    private Double preco;
 
-    @OneToMany(mappedBy = "categoria")
-    private List<Atividade> atividades = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 
-    public Categoria() {
+    public Atividade() {
     }
 
-    public Categoria(Integer id, String descricao) {
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
         this.id = id;
+        this.nome = nome;
         this.descricao = descricao;
+        this.preco = preco;
     }
 
     public Integer getId() {
@@ -34,6 +36,14 @@ public class Categoria {
         this.id = id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getDescricao() {
         return descricao;
     }
@@ -42,13 +52,21 @@ public class Categoria {
         this.descricao = descricao;
     }
 
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Categoria categoria = (Categoria) o;
-        return Objects.equals(id, categoria.id);
+        Atividade atividade = (Atividade) o;
+        return Objects.equals(id, atividade.id);
     }
 
     @Override
